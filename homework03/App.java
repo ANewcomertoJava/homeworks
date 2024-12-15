bd8042d35ddb9a320f0701587903f2720bd2599c
ANewcomertoJava <iit8610@gmail.com>
2024-12-15 19:08:03 +0000

    App

---
+++ homework03/App.java
@@ -0,0 +1,15 @@
+package scr;
+
+public class App {
+        public static void main(String[] args) {
+        // создание экземпляров класса Televisor
+        Televisor tv1 = new Televisor("Samsung", "UE50DU7100UXRU", 50);
+        Televisor tv2 = new Televisor("Sony", "KD-55X75K AF1", 54.6);
+        Televisor tv3 = new Televisor("Thoshiba", "100Z670NE", 100);
+
+        System.out.println(tv1.toString());
+        System.out.println(tv2.toString());
+        System.out.println(tv3.toString());
+    }
+}
+
