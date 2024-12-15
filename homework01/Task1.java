e4a498045a812aea8977dfae2be181fe27550d2a
ANewcomertoJava <iit8610@gmail.com>
2024-12-15 18:48:06 +0000

    Task1

---
+++ homework01/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+ public static void main(String[] args) {
+  int a = 8;
+  int b = 20;
+  int c = 34;
+  int d = 99;
+
+  // Выводим числа с тремя пробелами между ними
+  System.out.println(a + " " + b + " " + c + " " + d);
+ }
+}
