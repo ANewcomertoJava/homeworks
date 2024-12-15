0195ff8edda49bfe9a481bdf7bfe7d0802018603
ANewcomertoJava <iit8610@gmail.com>
2024-12-15 19:08:37 +0000

    Televisor

---
+++ homework03/Televisor.java
@@ -0,0 +1,51 @@
+package scr;
+/*
+Формулировка задания:
+Реализовать класс Телевизор. У класса есть поля, свойства и методы.
+Проверить работу в классе App, методе main.
+Обратить внимание на переопределение метода toString.
+*/
+
+public class Televisor {
+    private String manufacturer;
+    private String model;
+    private double diagonal;
+
+    // Конструктор для создания объекта телевизора.
+    public Televisor(String manufacturer, String model, double diagonal) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.diagonal = diagonal;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getDiagonal() {
+        return diagonal;
+    }
+    // Печатает информацию о телевизоре
+    public void printInfo() {
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Модель: " + model);
+        System.out.println("Диагональ: " + diagonal);
+    }
+    /* переопределение метода toString() */
+    @Override
+    public String toString() {
+        return "Производитель: " + manufacturer + " Модель: " + model + " Диагональ: " + diagonal;
+    }
+}
+
+
+
+
+
+
+
+
